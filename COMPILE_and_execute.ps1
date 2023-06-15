35968e805870ba03faeb229c8c063899f4e5a5b6
Colon101 <99831740+Colon101@users.noreply.github.com>
2023-06-16 00:00:25 +0300

    fixed some issues with bat file and replaced to powershell

--- COMPILE_and_execute.ps1
+++ COMPILE_and_execute.ps1
@@ -1,7 +1,11 @@
 winget install Python.Python.3.11
 pip install -r requirements.txt
+
 $sourceFile = "TERMiGUi.py"
-$outputName = "code.exe
+$outputName = "code.exe"
+
 pyinstaller --onefile --windowed --add-data "cheer.mp3;." --add-data "words.txt;." --add-data "code.ico;." --hidden-import pygame --icon code.ico --name $outputName $sourceFile
-del $outputName.spec
-start dist\code.exe
+
+Remove-Item -Path "$outputName.spec"
+
+Start-Process -FilePath "dist\$outputName"
