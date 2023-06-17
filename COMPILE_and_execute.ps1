4c2dee36dd780fd82ad711dbe8d891cea1ccca4d
Colon101 <kfirgoldman12@gmail.com>
2023-06-17 23:04:11 +0300

    fix typo

--- COMPILE_and_execute.ps1
+++ COMPILE_and_execute.ps1
@@ -4,7 +4,7 @@ pip install -r requirements.txt
 $sourceFile = "TERMiGUi.py"
 $outputName = "code.exe"
 
-pyinstaller --onefile --windowed --add-data "cheer.mp3;." --add-data "words.txt;." --add-data "code.png;." --add-data "jumpscare.png;." --add-data "jumpscare.mp4;." --hidden-import pygame --icon code.ico --name $outputName $sourceFile
+pyinstaller --onefile --windowed --add-data "cheer.mp3;." --add-data "words.txt;." --add-data "code.png;." --add-data "jumpscare.png;." --add-data "jumpscare.mp3;." --hidden-import pygame --icon code.ico --name $outputName $sourceFile
 
 Remove-Item -Path "$outputName.spec"
 
