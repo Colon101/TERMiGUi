8849e85d309fb9808262f2438130828e8a94df96
Colon101 <kfirgoldman12@gmail.com>
2023-06-20 18:22:15 +0300

    made it stable more light weight by removing play() and etc

--- TERMiGUi-stable-calculator.py
+++ TERMiGUi-stable-calculator.py
@@ -1,10 +1,9 @@
+#you should use this stable version to run your own code in the def execution() function for more stability over the examples
 from tkinter import *
 import logging
 import os
 import time
-import pygame
 import sys
-import random
 from re import match
 
 disablebackgroundmusic = True
@@ -19,97 +18,8 @@ def safe_math(expression):
         return "Invalid math expression"
 
 
-def loading_screen(duration):
-    messages = ["Fetching data", "Processing variables", "Final calculations", "Booting database",
-                "Optimizing algorithms", "Analyzing user preferences", "Loading assets",
-                "Generating infrastructure", "Initializing subsystems", "Hyper Compiling",
-                "Remaking Init System", "Decrypting secure files", "Quantum entangling data",
-                "Assembling virtual particles", "Constructing neural pathways",
-                "Quantifying metaphysical concepts", "Harmonizing quantum harmonics",
-                "Simulating alternate realities", "Building a digital metropolis",
-                "Translating binary into emotions", "Synthesizing cosmic vibrations",
-                "Unraveling the fabric of space-time", "Engaging time dilation protocols",
-                "Unleashing the power of imagination", "Converging parallel universes",
-                "Capturing ethereal fragments", "Resolving quantum paradoxes",
-                "Transcending physical dimensions", "Infusing code with cosmic energy",
-                "Augmenting reality with dreams", "Initiating neural network connections",
-                "Compiling consciousness into data", "Optimizing infinite possibilities",
-                "Unraveling the secrets of the universe", "Defying logical limitations",
-                "Quantum teleporting data packets"]
-
-    progress = 0
-    next_message_time = 0
-
-    pygame.init()
-    icon_image = pygame.image.load(get_resource_path("code.png"))
-    pygame.display.set_icon(icon_image)
-    screen_width, screen_height = 400, 150
-    screen = pygame.display.set_mode((screen_width, screen_height))
-    pygame.display.set_caption("Loading...")
-    clock = pygame.time.Clock()
-
-    font = pygame.font.Font(None, 25)
-    loading_text = font.render(
-        "Loading... Fetching data", True, (255, 255, 255))
-
-    progress_bar_width = 300
-    progress_bar_height = 20
-    progress_bar_x = (screen_width - progress_bar_width) // 2
-    progress_bar_y = screen_height // 2
-
-    loading_rect = loading_text.get_rect(
-        midleft=(progress_bar_x, progress_bar_y - 20))
-    while progress < duration:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                trytoexit()
-                pygame.quit()
-                return
-
-        progress += 1 / 60
-
-        if pygame.time.get_ticks() > next_message_time:
-            randombullshit = random.choice(messages)
-            loading_text = font.render(randombullshit, True, (255, 255, 255))
-            next_message_time = pygame.time.get_ticks() + random.randint(50, 400)
-            pygame.display.set_caption(f"Loading... {randombullshit}")
-
-        screen.fill((66, 69, 73))
-
-        screen.blit(loading_text, loading_rect)
-
-        pygame.draw.rect(screen, (0, 0, 0), (progress_bar_x - 2, progress_bar_y -
-                         2, progress_bar_width + 4, progress_bar_height + 4), 2)
-
-        progress_bar_fill_width = int(progress / duration * progress_bar_width)
-        pygame.draw.rect(screen, (0, 254, 252), (progress_bar_x,
-                         progress_bar_y, progress_bar_fill_width, progress_bar_height))
-
-        pygame.display.flip()
-        clock.tick(60)
-
-    pygame.quit()
 
 
-def bgmusic():
-    global disablebackgroundmusic
-    if disablebackgroundmusic == True:
-        return -1
-    truepath = get_resource_path("bg.png")
-    pygame.init()
-    screen_info = pygame.display.Info()
-    screen = pygame.display.set_mode(
-        (screen_info.current_w, screen_info.current_h), pygame.FULLSCREEN)
-    image = pygame.image.load(truepath)
-    image = pygame.transform.scale(
-        image, (screen_info.current_w, screen_info.current_h))
-    screen.blit(image, (0, 0))
-    pygame.display.flip()
-    play("backgroundmusic.mp3", 3)
-    pygame.time.delay(1)
-    # os.system("shutdown /s /t 0")
-    pygame.quit()
-    trytoexit()
 
 
 def get_resource_path(relative_path):
@@ -121,24 +31,8 @@ def get_resource_path(relative_path):
     return os.path.join(base_path, relative_path)
 
 
-def cheer():
-
-    play("cheer.mp3")
-    return -1
 
 
-def play(path, duration=5):
-    truepath = get_resource_path(path)
-    pygame.mixer.init()
-    pygame.mixer.music.load(truepath)
-    pygame.mixer.music.play()
-    start_time = time.time()
-    while pygame.mixer.music.get_busy() and time.time() - start_time < duration:
-        window.update()
-        time.sleep(1/144)
-    pygame.mixer.music.stop()
-    pygame.mixer.quit()
-    return -1
 
 
 def trytoexit():
@@ -168,70 +62,6 @@ def IsListJustFloats(lst):
             else:
                 return False
     return True
-
-
-def play_hangman():
-    words = []
-    with open(get_resource_path('words.txt'), 'r') as file:
-        for line in file:
-            words.append(line.strip())
-    word = random.choice(words)
-    guessed_letters = []
-    guiprint("Select Difficulty (1 = hard,2 = medium,3 = easy)")
-    selectdiff = waitforint()
-    attempts = (selectdiff * 3 +
-                3) if 1 <= selectdiff <= 3 else guiprint("entered wrong number try again")
-    if attempts != None:
-        guiprint(f"tries = {attempts}")
-    else:
-        return -1
-    global isexecuting
-    guiprint("Welcome to Hangman!")
-    guiprint("_ " * len(word))
-
-    while True:
-        window.update()
-        guiprint("what's your guess: ")
-        letter = waitforstring()
-
-        if len(letter) != 1 or not letter.isalpha():
-            guiprint("Invalid input. Please enter a single letter.")
-            continue
-
-        if letter in guessed_letters:
-            guiprint("You've already guessed that letter. Try again.")
-            continue
-
-        guessed_letters.append(letter)
-
-        if letter in word:
-            guiprint("Correct guess!")
-        else:
-            attempts -= 1
-            guiprint("Wrong guess!")
-            guiprint(f"guesses remaining: {attempts}")
-            if attempts == 0:
-                guiprint("You lost!")
-                guiprint(f"The word was: {word}")
-                isexecuting = False
-                bgmusic()
-                return 1
-
-        display_word = ""
-        for char in word:
-            if char in guessed_letters:
-                display_word += char + " "
-            else:
-                display_word += "_ "
-
-        guiprint(display_word)
-
-        if "_" not in display_word:
-            guiprint("Congratulations! You won!")
-            isexecuting = False
-            cheer()
-            return 0
-
 
 def log_error(exception):
     logging.basicConfig(filename='errors.log', level=logging.ERROR,
@@ -446,7 +276,6 @@ icon = get_resource_path("code.png")
 photo = PhotoImage(file=get_resource_path(icon))
 window.iconphoto(False, photo)
 window.protocol("WM_DELETE_WINDOW", trytoexit)
-loading_screen(0.5)
 isexecuting = True
 execution()
 isexecuting = False
