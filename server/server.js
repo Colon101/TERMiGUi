6ce7317ae5ba98a6a09b29b921af47fd6e5a9212
Colon101 <99831740+Colon101@users.noreply.github.com>
2023-07-01 17:06:55 +0300

    fixes

--- server/server.js
+++ server/server.js
@@ -18,7 +18,44 @@ db.run(`
 `);
 
 app.post('/', (req, res) => {
-    // ... Existing code for handling POST requests ...
+    const { username, score } = req.body;
+
+    if (!username || !score) {
+        res.status(400).send('Username and score are required.');
+        return;
+    }
+
+    db.get('SELECT score FROM scores WHERE username = ?', [username], (err, row) => {
+        if (err) {
+            console.error(err);
+            res.sendStatus(500);
+            return;
+        }
+
+        if (row) {
+            if (score > row.score) {
+                db.run('UPDATE scores SET score = ? WHERE username = ?', [score, username], (err) => {
+                    if (err) {
+                        console.error(err);
+                        res.sendStatus(500);
+                    } else {
+                        res.sendStatus(200);
+                    }
+                });
+            } else {
+                res.sendStatus(200);
+            }
+        } else {
+            db.run('INSERT INTO scores (username, score) VALUES (?, ?)', [username, score], (err) => {
+                if (err) {
+                    console.error(err);
+                    res.sendStatus(500);
+                } else {
+                    res.sendStatus(200);
+                }
+            });
+        }
+    });
 });
 
 app.get('/', (req, res) => {
@@ -35,50 +72,50 @@ app.get('/', (req, res) => {
                 .join('');
 
             const html = `
-        <!DOCTYPE html>
-        <html>
-          <head>
-            <title>Leaderboard</title>
-            <style>
-              body {
-                background-color: #222;
-                color: #fff;
-                font-size: 24px;
-              }
-
-              table {
-                border-collapse: collapse;
-                width: 100%;
-              }
-
-              th,
-              td {
-                border: 1px solid #fff;
-                padding: 8px;
-                text-align: left;
-                font-size: 24px;
-              }
-
-              h1 {
-                color: #fff;
-                font-size: 36px;
-              }
-            </style>
-          </head>
-          <body>
-            <h1>Leaderboard</h1>
-            <table>
-              <tr>
-                <th>Rank</th>
-                <th>Username</th>
-                <th>Score</th>
-              </tr>
-              ${leaderboard}
-            </table>
-            <button onclick="window.location.href='https://github.com/YourUsername/YourGame'">Go check out my game</button>
-          </body>
-        </html>
-      `;
+          <!DOCTYPE html>
+          <html>
+            <head>
+              <title>Leaderboard</title>
+              <style>
+                body {
+                  background-color: #222;
+                  color: #fff;
+                  font-size: 24px;
+                }
+  
+                table {
+                  border-collapse: collapse;
+                  width: 100%;
+                }
+  
+                th,
+                td {
+                  border: 1px solid #fff;
+                  padding: 8px;
+                  text-align: left;
+                  font-size: 24px;
+                }
+  
+                h1 {
+                  color: #fff;
+                  font-size: 36px;
+                }
+              </style>
+            </head>
+            <body>
+              <h1>Leaderboard</h1>
+              <p><a href="https://github.com/Colon101/TERMiGUi" target="_blank">Check out my program</a></p>
+              <table>
+                <tr>
+                  <th>Rank</th>
+                  <th>Username</th>
+                  <th>Score</th>
+                </tr>
+                ${leaderboard}
+              </table>
+            </body>
+          </html>
+        `;
 
             res.send(html);
         }
