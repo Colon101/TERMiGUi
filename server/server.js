6c65f5b2d63dfc79a8a741316a1801a5694fb673
Colon101 <kfirgoldman12@gmail.com>
2023-07-03 10:09:20 +0000

    some more shit

--- server/server.js
+++ server/server.js
@@ -15,8 +15,9 @@ db.run(`
     )
 `);
 
-app.use(express.json());
+const cheerio = require('cheerio'); // Import the cheerio package
 
+app.use(express.json());
 app.post('/', (req, res) => {
     const { username, score } = req.body;
 
@@ -58,26 +59,37 @@ app.post('/', (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  db.all('SELECT username, score FROM scores ORDER BY score DESC', (err, rows) => {
+    db.all('SELECT username, score FROM scores ORDER BY score DESC', (err, rows) => {
       if (err) {
-          return res.status(500).send('Internal Server Error.');
+        return res.status(500).send('Internal Server Error.');
       }
-
+  
       const leaderboard = rows.map((row) => ({ username: row.username, score: row.score }));
-
+  
       // Render the index.ejs file and pass the leaderboard data
       ejs.renderFile(path.join(__dirname, 'public', 'index.ejs'), { leaderboard }, (err, html) => {
-          if (err) {
-              return res.status(500).send('Internal Server Error.');
-          }
-
-          res.send(html); // Send the rendered HTML to the client
+        if (err) {
+          return res.status(500).send('Internal Server Error.');
+        }
+  
+        const $ = cheerio.load(html); // Load the HTML string into cheerio
+  
+        // Get the metadata tag and update its content
+        const metadataTag = $('meta[name="description"]');
+        const leaderboardRankings = leaderboard
+          .slice(0, 5)
+          .map((row, index) => `${index + 1}. ${row.username}`)
+          .join('\n');
+        metadataTag.attr('content', `Leaderboard Rankings:\n${leaderboardRankings}`);
+  
+        res.send($.html()); // Send the modified HTML to the client
       });
+    });
   });
-});
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(port, () => {
+  
+  // Serve static files from the 'public' directory
+  app.use(express.static(path.join(__dirname, 'public')));
+  
+  app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
-});
+  });
