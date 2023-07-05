f10d65f0638f541b6001356c0d7c21160ad541f1
Colon101 <kfirgoldman12@gmail.com>
2023-07-05 03:02:36 +0300

    final config for COMPILER

--- COMPILE_and_execute.ps1
+++ COMPILE_and_execute.ps1
@@ -1,5 +1,3 @@
-winget install Python.Python.3.11
-# trying to get stuff working
 $username = $env:USERNAME
 
 # Define the path to be added to the PATH variable
@@ -14,25 +12,12 @@ if ($currentPath -notlike "*$pythonPath*") {
     [Environment]::SetEnvironmentVariable("PATH", $newPath, "User")
 }
 
-
-
-
-# Install Python 3.11 using winget
-
-# Add Python Scripts folder to PATH
-
-
-
 # Install requirements.txt using pip
 "C:\Users\$username\AppData\Local\Programs\Python\Python311\Scripts\pip install -r requirements.txt"
 
 # Build executable using pyinstaller
 $sourceFile = "TERMiGUi.py"
-$outputName = "code.exe"
-
-$username = $env:USERNAME
 $outputName = "output"
-$sourceFile = ".\TERMiGUi.py"
 
 $pyinstallerPath = "C:\Users\$username\AppData\Local\Programs\Python\Python311\Scripts\pyinstaller"
 $cheerPath = "cheer.mp3"
@@ -42,7 +27,14 @@ $apikeyPath = ".apikey.txt"
 $spellgamePath = "spellgame.txt"
 $waltuhPath = "Waltuh.mp3"
 
-$pyinstallerArgs = "--onefile --windowed --add-data '$cheerPath;.' --add-data '$wordsPath;.' --add-data '$codeImagePath;.' --add-data '$apikeyPath;.' --add-data '$spellgamePath;.' --add-data '$waltuhPath;.' --hidden-import pygame --hidden-import spellchecker --icon code.ico --name $outputName $sourceFile"
+# Check if .apikey.txt file exists
+if (Test-Path $apikeyPath) {
+    # File exists, add it as a data file in PyInstaller
+    $pyinstallerArgs = "--onefile --windowed --add-data '$cheerPath;.' --add-data '$wordsPath;.' --add-data '$codeImagePath;.' --add-data '$apikeyPath;.' --add-data '$spellgamePath;.' --add-data '$waltuhPath;.' --hidden-import pygame --hidden-import spellchecker --icon code.ico --name $outputName $sourceFile"
+} else {
+    # File doesn't exist, exclude it from PyInstaller
+    $pyinstallerArgs = "--onefile --windowed --add-data '$cheerPath;.' --add-data '$wordsPath;.' --add-data '$codeImagePath;.' --add-data '$spellgamePath;.' --add-data '$waltuhPath;.' --hidden-import pygame --hidden-import spellchecker --icon code.ico --name $outputName $sourceFile"
+}
 
 Invoke-Expression "$pyinstallerPath $pyinstallerArgs"
 
@@ -52,5 +44,5 @@ Remove-Item -Path "$outputName.spec"
 # Run the generated executable
 Start-Process -FilePath "dist\$outputName"
 
-Write-Host "if an error occurred. Please try restarting"
+Write-Host "If an error occurred, please try restarting."
 Read-Host -Prompt "Press ENTER to exit"
