d8d56b9c22d29f29687f12e7e6b25b7724bbf2ac
Colon101 <kfirgoldman12@gmail.com>
2023-11-02 17:58:35 +0200

    it has been done

--- examples.py
+++ examples.py
@@ -3,10 +3,9 @@ from TERMiGUi import TERMiGUi
 
 def main():
     def mainProgram():
-        tg.guiprint("wow code started",)
-        tg.sleep(1)
-        tg.guiprint("wow code ended")
-    tg = TERMiGUi(programName="myprogram", mainFunction=mainProgram)
+        tg.print("examples will be here soon")
+    tg = TERMiGUi(programName="Examples", mainFunction=mainProgram,
+                  icon="https://cdn-icons-png.flaticon.com/512/1157/1157109.png")
 
     tg.mainloop()
 
